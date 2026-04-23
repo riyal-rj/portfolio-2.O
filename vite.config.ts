3399a0552f91a9d6ea2e84d5b8cad5083daf4382
Ritankar Jana <ritankar.jana.official@gmail.com>
2026-04-24 01:45:45 +0530

    Update the files for tanstack

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-
-export default defineConfig();
+export default defineConfig({
+  plugins: [tanstackStart(), nitro(), react()],
+});
